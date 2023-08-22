e116efd5dca36b431823ac0df0140fb329089c22
b4c1c <b4c1c@b4c1c-device.local>
2023-08-22 11:17:31 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import Title from "./Title";
+import menu from "./data";
+import Menu from "./Menu";
+import Categories from "./Categories";
+
+// const tempCategories = menu.map((item) => item.category);
+// console.log(tempCategories);
+
+// const tempSet = new Set(tempCategories);
+// console.log(tempSet);
+
+// const tempItems = ["all", ...tempSet];
+// console.log(tempItems);
+
+//one liner code - React Udemy-lesson 7.198
+
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+
+const App = () => {
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(menu);
+      return;
+    }
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+  return (
+    <main>
+      <section className="menu">
+        <Title text="Our Menu" />
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+};
+export default App;
